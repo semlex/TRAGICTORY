2bcb4496d5c4f6467b450a13e4eba6f6e5c13946
Semenov Alexey <alexpskov60@gmail.com>
2022-03-25 20:21:57 +0300

    [semlex] changed base url

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000/api/'
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken || "";
+const BASE_URL = 'http://tragictory.ru/api/'
 
 const getCurrentToken = () => {
    const user = JSON.parse(localStorage.getItem('persist:auth'))
