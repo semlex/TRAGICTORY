ffc4a0eb7c386a76d6571ff778fb6b160adadba4
Semenov Alexey <alexpskov60@gmail.com>
2022-03-26 19:10:42 +0300

    [semlex] orders smlfix

--- api/routes/order.js
+++ api/routes/order.js
@@ -119,9 +119,9 @@ router.delete('/delete/:id', verifyTokenAndAdmin, async (req, res) => {
 
 //GET USER ORDERS
 
-router.get('/user/:userId', verifyTokenAndAuthorization, async (req, res) => {
+router.get('/user/:id', verifyTokenAndAuthorization, async (req, res) => {
    try {
-      const orders = await Order.find({ user: req.params.userId }).sort({ createdAt: 'desc' })
+      const orders = await Order.find({ user: req.params.id }).sort({ createdAt: 'desc' })
       res.status(200).json(orders)
    } catch (err) {
       res.status(500).json(err)
